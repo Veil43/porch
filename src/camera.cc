26e499cc5e2d7579551e94dce5d1b40f5e014ad8
veil43 <glassarmourclass@gmail.com>
2025-05-23 21:35:14 +1000

    Computer migration

--- src/camera.cc
+++ src/camera.cc
@@ -1,2 +1,26 @@
 #include "camera.hh"
+#include "math.hh"
 
+void Camera::initialize() {
+    
+}
+
+void Camera::render(const Hittable& scene) {
+
+}
+
+color Camera::compute_ray_color(const ray& r, const Hittable& scene) const {
+    using math::Interval;
+
+    HitRecord record;
+    // if hit color in the normal else color in the sky
+    bool hit_success = scene.hit(r, Interval(0.0, math::infinity), record);
+    if (hit_success) {
+        color object_color = 0.5 * (record.normal + color(1.0));
+        return object_color;
+    }
+
+    vec3 unit_ray_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_ray_direction.y + 1.0);
+    return math::lerp(color(1.0, 1.0, 1.0), color(0.5, 0.7, 0.9), t);
+}
--- src/camera.hh
+++ src/camera.hh
@@ -5,10 +5,17 @@
 #include "color.hh"
 
 class Camera {
-
 public:
+    void render(const Hittable& world);
 
 private:
+    int m_image_height;
+    point3 m_center;
+    point3 pixel00;
+    vec3 delta_u;
+    vec3 delta_v;
+
+    void initialize();
     color compute_ray_color(const ray& r, const Hittable& scene) const;
 };
 
